a043d2ab08669ce3cec37de0942aff3f0b3c317a
Kam1runetz <notsoserious2017@gmail.com>
2020-10-31 01:58:50 +0300

    refactoring

--- include/config.hpp
+++ include/config.hpp
@@ -8,7 +8,7 @@
 const std::size_t CacheLineSize = 64;
 const std::size_t IterationsCount = 1000;
 const auto MyL1dCacheSize = 64 * 1024;
+[[maybe_unused]] const auto MyL2CacheSize = 512 * 1024;
 const auto MyL3CacheSize = 3 * 1024 * 1024;
-[[maybe_unused]] const auto GCC_PrefetchEnable = true;
 
 #endif  // INCLUDE_CONFIG_HPP_
