cd507ca646f058926d2dd12af9946303eb543b9b
Kam1runetz <notsoserious2017@gmail.com>
2020-11-08 23:58:57 +0300

    cmake edited

---
+++ include/CSVPrinter.hpp
@@ -0,0 +1,8 @@
+//
+// Created by w1ckedente on 08.11.2020.
+//
+
+#ifndef CACHE_CSVPRINTER_HPP
+#define CACHE_CSVPRINTER_HPP
+
+#endif  // CACHE_CSVPRINTER_HPP
---
+++ sources/CSVPrinter.cpp
@@ -0,0 +1,3 @@
+//
+// Created by w1ckedente on 08.11.2020.
+//
